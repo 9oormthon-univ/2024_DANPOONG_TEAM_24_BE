9fe25f2d05bb2ba7159ee28083e4088e2d0c3f66
김규리 <kimkyuree1@naver.com>
2024-11-20 04:41:00 +0900

    Add: Store, Category, Recipe Repository

---
+++ src/main/java/com/ieum/be/repository/RecipeRepository.java
@@ -0,0 +1,9 @@
+package com.ieum.be.repository;
+
+import com.ieum.be.domain.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
+}
---
+++ src/main/java/com/ieum/be/repository/StoreRepository.java
@@ -0,0 +1,9 @@
+package com.ieum.be.repository;
+
+import com.ieum.be.domain.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Integer> {
+}
